81a8e609a44a6686a31d53daa4bc747245baea23
anastasia <anyasirman@outlook.com>
2022-08-22 15:38:32 -0400

    user api

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,4 +2,19 @@ class UserController < ApplicationController
     def index
         render json:User.all
     end
+
+    def delete_user
+        User.find(params[:id]).destroy
+        render json: {message: "User #{params[:id]} deleted"}
+    end
+
+    def edit_user
+        User.find(params[:id]).update(username: params[:username], password: params[:password],profile_pic: params[:profile_pic], email: params[:email]) 
+        render json:{message:"Updated Entry #{params[:id]}"}
+    end 
+
+    def new_user 
+        User.create!(params[:id]).update(username: params[:username], password: params[:password],profile_pic: params[:profile_pic], email: params[:email]) 
+        render json:{message:"Created Entry!"}
+    end
 end
