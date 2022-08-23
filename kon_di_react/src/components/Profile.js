bc855bfe8c45537a392d2fa85775a35da49b5d25
daragmas <daragmas@gmail.com>
2022-08-23 16:04:56 -0400

    added list rerendering when deleting an entry

--- kon_di_react/src/components/Profile.js
+++ kon_di_react/src/components/Profile.js
@@ -7,12 +7,13 @@ const Profile = () => {
 
     let params = useParams()
 
+    const getEntries = async () => {
+        const req = await fetch(`http://127.0.0.1:3000/entries/2`) //TODO: Change to dynamic id!!!
+        const res = await req.json()
+        setEntries(res)
+    }
+
     useEffect(() => {
-        const getEntries = async () => {
-            const req = await fetch(`http://127.0.0.1:3000/entries/2`) //TODO: Change to dynamic id!!!
-            const res = await req.json()
-            setEntries(res)
-        }
         getEntries()
     }, [])
 
@@ -22,15 +23,18 @@ const Profile = () => {
         console.log(entries[e.target.id])
     }
 
+    const handleDeleteClick = () => {
+        window.confirm('Delete Entry?') ? fetch(`http://127.0.0.1:3000/entries/delete/${selectedEntry.id}`, { method: "DELETE" }) : console.log('No Delete')
+        getEntries()
+    }
+
     const entries_lis = entries.length !== 0 ?
         entries.map((entry, index) => <li key={entry.title} className='profile-entry'>
             <span className='profile-entry-date'>{entry.created_at}</span> | <span className='profile-entry-title' id={index} onClick={handleEntryLiClick}>{entry.title}</span>
         </li>) :
         <li id='no-entries'>You have no entries</li>
 
-    const handleDeleteClick = () => {
-        window.confirm('Delete Entry?') ? fetch(`http://127.0.0.1:3000/entries/delete/${selectedEntry.id}`) : console.log('No Delete')
-    }
+   
 
     return (
         <div>
