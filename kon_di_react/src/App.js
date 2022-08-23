c73cade79d5bdb75f22ace97f718b59cfe71011c
anastasia <anyasirman@outlook.com>
2022-08-23 09:10:21 -0400

    added form functionality

--- kon_di_react/src/App.js
+++ kon_di_react/src/App.js
@@ -2,12 +2,13 @@ import NavBar from './components/Navbar';
 import './App.css';
 import Home from './components/Home';
 import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <Login/>
+      <Register/>
     </div>
   );
 }
