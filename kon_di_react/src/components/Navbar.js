1c6a11525749153681cbef853eabeb2ee7ea8f46
anastasia <anyasirman@outlook.com>
2022-08-23 12:54:07 -0400

    added links to all pages

--- kon_di_react/src/components/Navbar.js
+++ kon_di_react/src/components/Navbar.js
@@ -1,11 +1,14 @@
+import {NavLink} from 'react-router-dom'
+
 const NavBar = () => {
 return (
     <nav className="navbar">
         <div className='logo'>LOGO</div>
         <ul className='nav-list'>
-            <li className='nav-items'>Home</li>
-            <li className='nav-items'>About</li>
-            <li className='nav-items'>LOGIN</li>
+            <NavLink to='/'><li className='nav-items'>Home</li></NavLink>
+            <NavLink to='/about'><li className='nav-items'>About</li></NavLink>
+            <NavLink to='/login'><li className='nav-items'>LOGIN</li></NavLink>
+            <NavLink to='/register'><li className='nav-items'>REGISTER</li></NavLink>
         </ul>
     </nav>
 )
