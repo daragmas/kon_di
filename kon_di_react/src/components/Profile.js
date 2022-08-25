9d10e3436ab8c14fe142a48407aeafc1a26bbab8
daragmas <daragmas@gmail.com>
2022-08-25 16:04:05 -0400

    styling

--- kon_di_react/src/components/Profile.js
+++ kon_di_react/src/components/Profile.js
@@ -6,6 +6,8 @@ import '../App.css'
 const Profile = ({ loginState }) => {
     const [entries, setEntries] = useState([])
     const [selectedEntry, setSelectedEntry] = useState({})
+    const [refresh, setRefresh] = useState(false)
+
     let navigate = useNavigate()
 
 
@@ -23,6 +25,7 @@ const Profile = ({ loginState }) => {
                 body: JSON.stringify({ user_id: user_id })
             })
             const res = await req.json()
+            console.log(res)
             setEntries(res)
         }
         catch {
@@ -33,27 +36,36 @@ const Profile = ({ loginState }) => {
 
     useEffect(() => {
         getEntries()
-    }, [])
+    }, [refresh])
 
     const handleEntryLiClick = (e) => {
         setSelectedEntry(entries[e.target.id])
     }
 
-    const handleDeleteClick = () => {
-        window.confirm('Delete Entry?') ? fetch(`http://localhost:3000/entries/delete/${selectedEntry.id}`, { method: "DELETE" }) : console.log('No Delete')
+    const handleDeleteClick = async () => {
+        window.confirm('Delete Entry?') ? await fetch(`http://localhost:3000/entries/delete/${selectedEntry.id}`, { method: "DELETE" }) : console.log('No Delete')
         getEntries()
+        setSelectedEntry({})
+        setRefresh(prev => !prev)
     }
 
     const formatCreatedAt = (entry) => {
-        let created_atDateSplit = entry.created_at.split('T')[0].split('-')
-        return (created_atDateSplit[1] + "/" + created_atDateSplit[2] + "/" + created_atDateSplit[0])
+        try {
+            let created_atDateSplit = entry.created_at.split('T')[0].split('-')
+            return (created_atDateSplit[1] + "/" + created_atDateSplit[2] + "/" + created_atDateSplit[0])
+        }
+        catch {
+            console.log('No Entry')
+        }
     }
 
-    const entries_lis = entries.length !== 0 ?
-        entries.map((entry, index) => <li key={entry.title} className='profile-entry'>
-            <span className='profile-entry-date'>{formatCreatedAt(entry)}</span> | <span className='profile-entry-title' onClick={handleEntryLiClick}><b id={index}>{entry.title}</b></span>
-        </li>) :
-        <li id='no-entries'>You have no entries</li>
+    const entries_lis = (entries) => {
+        return entries.length !== 0 ?
+            entries.map((entry, index) => <li key={entry.title} className='profile-entry'>
+                <span className='profile-entry-date'>{formatCreatedAt(entry)}</span> | <span className='profile-entry-title' onClick={handleEntryLiClick}><b id={index}>{entry.title}</b></span>
+            </li>) :
+            <li id='no-entries'>You have no entries</li>
+    }
 
     return (
         <>
@@ -67,29 +79,29 @@ const Profile = ({ loginState }) => {
                 <div id="profile-entries-list-container">
                     <h3 id='profile-entries-label'>Entries</h3>
                     <ul className='entries-list'>
-                        {entries_lis}
+                        {entries_lis(entries)}
                     </ul>
                 </div>
                 <div id='profile-selected-entry-container'>
                     <h3 className='profile-selected-entry-title'>{selectedEntry.title}</h3>
-                    <small>{selectedEntry.created_at}</small>
+                    <small>{formatCreatedAt(selectedEntry)}</small>
                     <p>{selectedEntry.content}</p>
                     {selectedEntry.id ?
                         (
                             <>
                                 <button id="profile-entry-delete-button" onClick={handleDeleteClick}>Delete Entry</button>
                                 <NavLink style={{
-                                    alignItems:"center",
+                                    alignItems: "center",
                                     backgroundColor: "#73A9AD",
                                     border: "3px solid #A1E6EB",
                                     borderRadius: "2px",
                                     color: "white",
-                                    display: "flex",
-                                    flexDirection:"row",
-                                    justifyContent:"center",
+                                    display: "inline-block",
+                                    fontSize: "small",
+                                    justifyContent: "center",
                                     textAlign: "center",
-                                    textDecoration:'none',
-                                    width: "25%"
+                                    textDecoration: 'none',
+                                    width: "30%"
                                 }} to={`/entry/${selectedEntry.id}`}>Edit Entry</NavLink>
                             </>
                         ) :
