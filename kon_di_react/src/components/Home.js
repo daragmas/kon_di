3d49ddc0d9b13c91e33422275ecce85631775c4a
anastasia <anyasirman@outlook.com>
2022-08-25 15:00:57 -0400

    styling

--- kon_di_react/src/components/Home.js
+++ kon_di_react/src/components/Home.js
@@ -1,20 +1,21 @@
 import '../Home.css'
 import '../App.css'
-
+import {NavLink} from 'react-router-dom'
 const Home = () => {
     return (
         <div className="home">
             <div className='hero'>
-                {/* <div className='hero-container'>
-                <h3>Need Privacy?</h3>
-                <p>Are you sick of people going through your diary with no respect for your personal space or privacy? Join in our mission to protect your privacy.</p>
+                <div className='hero-container'>
+                <h3>Guard your dairy</h3>
+                <p>Fully confidental, fully encrypted, fully protected.</p>
                 <div className='button-container'>
-                <button>Join now for Free</button>
+                <NavLink style={{ textDecoration: 'none' }} to='/register'><button>Join now for Free</button></NavLink>
+                
                 </div>
                 </div>
                 <div className='hero-img-div'>
                     <img src="safe.svg"/>
-                </div> */}
+                </div>
             </div>
 
         </div>
