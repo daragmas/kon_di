b10ad4a8bc3edced2271e74ae7007d30fac617ca
anastasia <anyasirman@outlook.com>
2022-08-26 10:55:30 -0400

    added about page

--- kon_di_react/src/App.js
+++ kon_di_react/src/App.js
@@ -6,6 +6,7 @@ import Profile from './components/Profile';
 import Register from './components/Register';
 import Entry from './components/Entry'
 import Footer from './components/Footer';
+import About from './components/About';
 import { BrowserRouter as Router, Route, NavLink, Routes } from "react-router-dom";
 import { useState } from 'react';
 
@@ -14,7 +15,6 @@ function App() {
   const [loggedIn, setLoggedIn] = useState((document.cookie=='hash=' || document.cookie =='') ? false : true)
 
   const handleLoginChange = (bool) => {
-    // console.log("bool", bool)
     setLoggedIn(bool)
   }
 
@@ -28,6 +28,7 @@ function App() {
           <Route exact key={3} path='/register'  element={<Register onLoginChange={handleLoginChange}/>} />
           <Route exact key={4} path='/signup' element={<Register onLoginChange={handleLoginChange}/>} />
           <Route exact key={5} path='/profile' element={<Profile loginState={loggedIn}/>} />
+          <Route exact key={6} path='/about' element={<About/>} />
           <Route key={6} path='/entry' element={<Entry />}>
             <Route exact key={7} path=':entryid' />
           </Route>
